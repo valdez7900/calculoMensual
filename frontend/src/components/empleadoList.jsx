baba622d29fc6aafea6ae33546860e2c693efc13
valdez7900 <lave790@gmail.com>
2022-10-07 23:13:03 -0500

    creando tabla para mostra los empleados

--- frontend/src/components/empleadoList.jsx
+++ frontend/src/components/empleadoList.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import useSWR, { useSWRConfig } from "swr";
 
-const empleadoList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const EmpleadoList = () => {
+    const { mutate } = useSWRConfig();
+    const fetcher = async () => {
+        const response = await axios.get("http://localhost:5000/empleados");
+        return response.data;
+    };
+
+    const { data } = useSWR("empleados", fetcher);
+    if (!data) return <h2>Cargando...</h2>;
+
+    const deleteProduct = async (empleadoId) => {
+        await axios.delete(`http://localhost:5000/empleados/${empleadoId}`);
+        mutate("empleados");
+    };
+
+    return (
+        <div className="flex flex-col mt-5">
+          <div className="w-full">
+            <Link
+              to="/add"
+              className="bg-green-500 hover:bg-green-700 border border-slate-200 text-white font-bold py-2 px-4 rounded-lg"
+            >
+              Agregar Nuevo Empleado
+            </Link>
+            <div className="relative shadow rounded-lg mt-3">
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-100">
+                  <tr>
+                    <th className="py-3 px-1 text-center">No</th>
+                    <th className="py-3 px-6">Nombre Empleado</th>
+                    <th className="py-3 px-6">Rol Empleado</th>
+                    <th className="py-3 px-6">Tipo Empleado</th>
+                    <th className="py-3 px-1 text-center">Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((empleado, index) => (
+                      <tr className="bg-white border-b" key={empleado.id}>
+                      <td className="py-3 px-1 text-center">{index + 1}</td>
+                      <td className="py-3 px-6 font-medium text-gray-900">
+                        {empleado.nombreEmpleado}
+                      </td>
+                      <td className="py-3 px-6">{empleado.rolEmpleado}</td>
+                      <td className="py-3 px-6">{empleado.tipoEmpleado}</td>
+                      <td className="py-3 px-1 text-center">
+                        <Link
+                          to={`/edit/${empleado.id}`}
+                          className="font-medium bg-blue-400 hover:bg-blue-500 px-3 py-1 rounded text-white mr-1"
+                        >
+                          Editar
+                        </Link>
+                        <button
+                          onClick={() => deleteProduct(empleado.id)}
+                          className="font-medium bg-red-400 hover:bg-red-500 px-3 py-1 rounded text-white"
+                        >
+                          Eliminar
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      );
 }
 
-export default empleadoList
+export default EmpleadoList
